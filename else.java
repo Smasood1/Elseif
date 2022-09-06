eb0d4439726aabe9b1bf805e1f609f04e187fde7
Smasood1 <saadmasood219@gmail.com>
2022-09-06 14:52:14 -0400

    classwork push to repo

---
+++ else.java
@@ -0,0 +1,21 @@
+public class ShapeFactory {
+    public static void main(String[] args) {
+
+        double grade = Math.floor(Math.random() * 101);
+
+        if (grade >= 90){
+            System.out.println("You got an A!");
+        }else if (grade >= 80){
+            System.out.println("You got a B");
+        }else if (grade >= 70){
+            System.out.println("You got a C");
+        }else if (grade >= 60){
+            System.out.println("You got a D");
+        }else if (grade >= 50){
+            System.out.println("You got a F");
+        }else{
+            System.out.println("You failed miserably");
+        }
+
+    }
+}
